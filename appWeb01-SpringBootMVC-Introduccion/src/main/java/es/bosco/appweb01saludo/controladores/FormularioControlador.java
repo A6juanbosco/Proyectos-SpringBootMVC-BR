bc5c49db871e61891249c86dde50f867c036c1f0
Bautista R A <bautista@iesjuanbosco.es>
2024-01-02 09:49:53 +0100

    1ª versiond

--- appWeb01-SpringBootMVC-Introduccion/src/main/java/es/bosco/appweb01saludo/controladores/FormularioControlador.java
+++ appWeb01-SpringBootMVC-Introduccion/src/main/java/es/bosco/appweb01saludo/controladores/FormularioControlador.java
@@ -10,22 +10,18 @@ import org.springframework.web.bind.annotation.PostMapping;
 @Controller
 public class FormularioControlador {
 
-    @GetMapping("/formulario")
+    @GetMapping("/formulario")  //ir al formulario para la entrada de datos
     public String mostrarFormulario(Model model) {
         model.addAttribute("datos", new Persona());  //datos es  el objeto "vehículo" de transporte. (Va instanciado)
         return "formularioEntrada";
     }
 
-    @PostMapping("/procesar")
+    @PostMapping("/procesar") //simplemente pasamos la persona recibida a formularioSalida
     public String procesarFormulario(@ModelAttribute Persona persona, Model model) {
-        // Aquí podrías realizar acciones con los datos, como almacenarlos en una base de datos.
+        // Realizar acciones con los datos, como almacenarlos en una base de datos.
         // En este ejemplo, simplemente los pasamos a la página de salida.
         model.addAttribute("datos", persona);
         return "formularioSalida";
     }
-    @GetMapping("/salida")
-    public String mostrarSalida(@ModelAttribute Persona datos, Model model) {
-        model.addAttribute("datos", datos);
-        return "formularioSalida";
-    }
+
 }
